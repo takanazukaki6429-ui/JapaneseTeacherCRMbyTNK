29f3f559d8a2132aa4faff4d41e1147d3e132dc9
あなたのGitHubユーザー名 <GitHubgit config --global user.name GitHubgit config --global user.email GitHubgit push -u origin maingit push -u origin maingit push -u origin maingit push -u origin maingit push -u origin mainpwdlspwdls2026-02-01 16:58:34.044 [info]  git check-ig2A;2D;2Dcd /Users/tanakakazuki/Desktop/FitPartnercat .env.localcd /Users/tanakakazuki/Desktop/FitPartnercat .env.local>
2026-02-20 00:21:13 +0900

    fix: resolve Sentry build error by merging options

--- next.config.ts
+++ next.config.ts
@@ -20,22 +20,10 @@ export default withSentryConfig(content(nextConfig), {
   silent: true,
   org: "nihongo-teacher-crm",
   project: "nihongo-teacher-crm",
-}, {
+
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Transpiles SDK to be compatible with IE11 (increases bundle size)
-  transpileClientSDK: true,
-
   // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
   tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
 });
